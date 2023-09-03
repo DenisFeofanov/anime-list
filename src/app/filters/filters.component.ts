bd49671fa001ac50d966b60369f9ed2aa7a945c3
DenisFeofanov <omail4me@gmail.com>
2023-09-03 20:10:51 +0500

    add css classes for select visibility

--- src/app/filters/filters.component.ts
+++ src/app/filters/filters.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./filters.component.css'],
 })
 export class FiltersComponent {
-  isCheckboxesVisible: boolean = false;
+  areCheckboxesVisible: boolean = false;
 }
